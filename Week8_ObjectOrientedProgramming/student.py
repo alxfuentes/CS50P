de9d3b786b37b23f8c76164e0a3136fb873b7c5f
Alejandro Fuentes <alxfuentes@msn.com>
2026-03-06 15:26:23 -0600

    Added Property getters and setters to class

--- Week8_ObjectOrientedProgramming/student.py
+++ Week8_ObjectOrientedProgramming/student.py
@@ -1,16 +1,57 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus=None):
         self.name = name
         self.house = house
+        self.patronus = patronus
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not value:
+            raise ValueError("Name cannot be empty")
+        self._name = value
+
+    @property
+    def house(self):
+        return self._house
+    
+    @house.setter
+    def house(self, value):
+        if value not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = value
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    def charm(self):
+        if self.patronus:
+            match self.patronus.lower():
+                case "stag":
+                    return f"{self.name} casts a stag patronus!"
+                case "otter":
+                    return f"{self.name} casts an otter patronus!"
+                case "hare":
+                    return f"{self.name} casts a hare patronus!"
+                case _:
+                    return "🪄"
+        else:
+            return f"{self.name} has no patronus to cast."
+
     
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
+    print(student.charm())
     
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus = input("Patronus (optional): ")
+    return Student(name, house, patronus)
 
 if __name__ == "__main__":
     main()
