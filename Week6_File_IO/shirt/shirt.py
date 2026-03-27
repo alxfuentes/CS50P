804f530e3fe21c495871aa7dcce7304f11c8cc69
Alejandro Fuentes <alxfuentes@msn.com>
2026-03-27 08:05:45 -0600

    Added Pillow to shirt.py

---
+++ Week6_File_IO/shirt/shirt.py
@@ -0,0 +1,39 @@
+import sys
+import os
+from PIL import Image
+
+def main():
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+
+    if len(sys.argv) != 3:
+        sys.exit("Too few command-line arguments")
+
+    filename = sys.argv[1]
+    output_filename = sys.argv[2]
+
+    fileext = os.path.splitext(filename)[1]
+    output_fileext = os.path.splitext(output_filename)[1]
+
+    print(fileext)
+    print(output_fileext)
+
+    if fileext != output_fileext:
+        sys.exit("Input and output have different extensions")
+
+    if not os.path.isfile(filename):
+        sys.exit("File does not exist")
+
+    try:
+        with Image.open(filename) as before:
+            with Image.open("shirt.png") as shirt:
+                size = shirt.size
+                before = before.resize(size)
+                before.paste(shirt, shirt)
+                before.show()
+            
+    except Exception as e:
+        sys.exit(f"Error on file access: {e}")
+
+if __name__ == "__main__":
+    main()
